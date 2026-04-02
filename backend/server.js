00f1364aa8707647ffa7280bd29c61ecbea92085
SHAHITH AHAMED <shahithahamed.413@gmal.com>
2026-04-02 16:55:02 +0530

    up to date

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/api/message", (req, res) => {
+    res.json({ message: "Hello from Backend 🎉" });
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
